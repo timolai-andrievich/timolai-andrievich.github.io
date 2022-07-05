010c739155817f44def6c6b3685923ab41fad5b1
timolai-andrievich <timolai.andrievitch@ya.ru>
2022-07-05 13:20:56 +0300

    Ran prettier, added all necessary pages

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import IP from '$lib/IP.svelte';
+</script>
+
+<header>
+	<nav>
+		<div class="nav-link-container">
+			<a href="/" class="nav-link">Main page</a>
+		</div>
+		<div class="nav-link-container">
+			<a href="projects" class="nav-link">Projects</a>
+		</div>
+	</nav>
+</header>
+<slot />
+
+<footer>
+	&copy; 2022
+	<IP />
+</footer>
+
+<style>
+	footer {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		font-size: 9px;
+	}
+	:root {
+		margin-top: 7%;
+		margin-left: 10%;
+		margin-right: 10%;
+		font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode',
+			Geneva, Verdana, sans-serif;
+	}
+	nav {
+		text-align: center;
+		display: flex;
+		justify-content: space-evenly;
+	}
+	.nav-link-container {
+		flex: 1;
+	}
+	.nav-link {
+		color: black;
+		text-decoration: none;
+		font-size: 32px;
+	}
+	a {
+		border-bottom: 2px solid transparent;
+		text-decoration: none;
+		transition: 0.4s;
+	}
+	a:visited,
+	a:link,
+	a:active {
+		color: inherit;
+	}
+	a:hover {
+		border-bottom: 2px solid black;
+	}
+</style>
---
+++ src/routes/projects.svelte
@@ -0,0 +1,27 @@
+<main>
+	<div class="project-paragraph">
+		<h2 class="project-header">
+			<a href="https://github.com/timolai-andrievich/eo-strings">eo-strings</a>
+		</h2>
+		<p class="project-body">
+			A library for the string-related operations for the <a
+				href="https://github.com/objectionary/eo"
+				style="font-weight: bold">EO programming language</a
+			>.
+		</p>
+	</div>
+</main>
+
+<style>
+	a,
+	a:link,
+	a:active,
+	a:visited {
+		text-decoration: none;
+		font-weight: bold;
+		color: inherit;
+	}
+	a:hover {
+		text-decoration: underline;
+	}
+</style>

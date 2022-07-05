3418687b4ffe775d335ddfbe1086599652b90b23
timolai-andrievich <timolai.andrievitch@ya.ru>
2022-07-05 12:38:00 +0300

    Switched to svelte-kit

--- src/IPJSON.ts
+++
@@ -1,3 +0,0 @@
-export interface IpJson {
-    ip: string,
-}
--- src/Projects.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import Project from "./Project.svelte";
-    export let projects;
-</script>
-
-{#each projects as [header, description]}
-    <Project header={header} description={description} />
-{/each}
